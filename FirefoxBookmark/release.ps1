8d95e84ba26e1a24031db499d42491f4b43365dd
NORTCHOT <153174477+NORTCHOT@users.noreply.github.com>
2026-03-22 16:12:50 +0300

    Added Russian(ru-RU) translation

--- FirefoxBookmark/release.ps1
+++ FirefoxBookmark/release.ps1
@@ -21,7 +21,8 @@ foreach ($arch in $archs) {
 		"$releasePath/$assembly.deps.json",
 		"$releasePath/$assembly.dll",
 		"$releasePath/plugin.json",
-		"$releasePath/Images"
+		"$releasePath/Images",
+		"$releasePath/ru-RU"
 	)
 	Copy-Item $items "$tempDir" -Recurse -Force
 	Compress-Archive "$tempDir" "./out/$name-$version-$arch.zip" -Force
